4d1c7190e878b483bccecf135e17aca63feb24e4
Youssef Adouiri Alaoui <86375309+Youssef-ADOUIRI@users.noreply.github.com>
2021-10-21 01:24:56 +0100

    Normal ( Gaussian ) random distribution implementation

--- Matrix/matrix.h
+++ Matrix/matrix.h
@@ -4,16 +4,20 @@
 #include <vector>
 #include <functional>
 
+typedef unsigned int uint;
+
 class Matrix
 {
 
 private:
-    unsigned int nCols;
-    unsigned int nRows;
+    
     std::vector<double> matrix;
     static void f_random(double &x);
+    unsigned int nCols;
+    unsigned int nRows;
 
 public:
+
     Matrix(const size_t cols_num, const size_t row_num);
     Matrix(Matrix const &copy);
     Matrix &operator=(Matrix const &copy);
@@ -21,6 +25,8 @@ public:
     //Matrix& Matrix::operator=(Matrix const& copy);
     ~Matrix();
 
+    size_t getCols();
+    size_t getRows();
     void Reset(const size_t cols_num, const size_t row_num);
 
     void print() const;
@@ -37,7 +43,7 @@ public:
 
     // static bool operator==(Matrix const& m1, Matrix const& m2);
     // static Matrix& operator+(Matrix const& m1, Matrix const& m2);
-
+    void nRand(double local, double scale);
     void randomize();
 
     void apply(std::function<void(double &)> func);
@@ -46,12 +52,17 @@ public:
 
     void multiply_by(double const &multiplying_Num);
 
+    static Matrix normal_multiplication(Matrix const &m1, Matrix const &m2);
+
     double *to_ptr(bool arrangement_type);
     void fromArray(double arr[], size_t size);
     void fromVector(std::vector<double> arr);
     std::vector<double> toVector();
     friend bool operator==(Matrix const &m1, Matrix const &m2);
 };
+
+double Random_normal_disturbution(double local, double scale);
+std::pair<double, double> normal_distrbution_h(double x, double y, double mean, double scale);
 
 bool operator==(Matrix const &m1, Matrix const &m2);
 bool operator!=(Matrix const &m1, Matrix const &m2);
